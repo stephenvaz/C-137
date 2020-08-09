c52a2794654998eeb26332ac5badcb7ecbced386
stephenvaz03 <54773712+stephenvaz03@users.noreply.github.com>
2020-08-09 22:10:01 +0530

    Create it1.cpp
    v1.0

---
+++ IncomeTax/it1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float income;
+    char name;
+    cout << "Client Name:"; cin >> name;
+    cout <<  "Hi!" << name << "Please enter you Annual Income:Rs."; cin >> income;
+    while (income<250000)
+    {
+        
+    }
+     
+     
+}
