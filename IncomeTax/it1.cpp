e74c079c0335a4661b2aa10cf96560e31771990b
stephenvaz03 <54773712+stephenvaz03@users.noreply.github.com>
2020-08-10 11:38:38 +0530

    v1.2
    broken

--- IncomeTax/it1.cpp
+++ IncomeTax/it1.cpp
@@ -4,21 +4,41 @@ using namespace std;
 int main(){
     float x;
     string name;
+    int age;
     cout << "Client Name:"; cin >> name;
+    cout << "Client Age:"; cin >> age;
     cout <<  "Hi!" << name << ", \nPlease enter you Annual Income:Rs."; cin >> x;
-    while (x<250000)
+    while ((age<=60))
+    {
+    {
+        while (x<250000)
     {
         cout << "No Tax will be levied" << "\nFinal Income (After Deduction) is Rs." << x;
         break;
     }
-    while (x>=250001 && x<500000 )
+    while (x>=250001 && x<=500000 )
     {
         x=x-x*0.05;
         cout << "Tax Levied=5%" << "\nFinal Income (After Deduction) is Rs." << x;
         break;
     }
-
+    while (x>=500001 && x<=750000)
+    {
+        x=x-12500-(0.1*x);
+        cout << "Tax Levied=10% + 12500" <<"\nFinal Income (After Deduction) is Rs." << x;
+        break;
+    }
+    }
+    }
+    while (age>60 && x<=80)
+    {
+         while (x<300000)
+        {
+            cout << "No Tax wil be levied";
+        }
+    }
     
-     
-     
+    
 }
+    
+   
