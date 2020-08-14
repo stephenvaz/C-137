14d57661bed81230ac5335267017bb222147aa71
stephenvaz03 <54773712+stephenvaz03@users.noreply.github.com>
2020-08-14 17:37:15 +0530

    v1.0c
    Please check the pdf and use data for variables

--- udit/ud.cpp
+++ udit/ud.cpp
@@ -1,22 +1,16 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
 int main(){
-     const double Government_Rate = 0.15 ,STATE_RATE = 0.05,OVERTIME_RATE = 1.50;
-      int hours;
-      double rate, state , government, netPay, gross;
-      
-
+      float Government_Rate = 0.15 ,STATE_RATE = 0.05,OVERTIME_RATE = 1.50, rate, state, government, netPay, gross,hours;
       cout <<"Welcome to pay calculator";
 
-       
       do
       {
             cout << "\nPlease enter a value between 10 and 20 for Hourly rate: ";
             cin >> rate;
 
-      }while (rate < 10 || rate>20);
-
+      }
+      while (rate < 10 || rate>20);
 
       hours = 0;
       while (hours < 1 || hours > 60)
@@ -42,8 +36,4 @@ int main(){
            << "Government tax: " << government << " at 15%" << endl
            << "State tax:  \t" << state << " at 5%" << endl
            << "NetPay: \t" << netPay << endl;
-
-
-
-
 }
