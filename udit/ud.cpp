68f2b4db81f33525caae5e1a12275746afa52ee1
Atharv56 <69430707+Atharv56@users.noreply.github.com>
2020-08-14 18:02:24 +0530

    Update ud.cpp

--- udit/ud.cpp
+++ udit/ud.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
-int main(){
-     const double Government_Rate = 0.15 ,STATE_RATE = 0.05,OVERTIME_RATE = 1.5;
-      int hours;
-      double rate, state , government, netPay, gross;
-      cout << "Calculate Your Anual Pay"
-      cout << "xyz" << setprecision(2)<< Government_Rate;
+void main(){
+     const double Government_Rate = 0.15, STATE_RATE = 0.05, OVERTIME_RATE = 1.5;
 
-      co
-
-      }
+     INT H
+     }
