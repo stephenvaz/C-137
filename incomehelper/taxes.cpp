16ad368be14f2e38dc46b4c805df574ed6cf8fbe
kvagadia21 <kishan.vagadia@gmail.com>
2020-09-09 14:59:41 +0530

    start of report card

---
+++ incomehelper/taxes.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    string name;
+    double income;
+    cout<<"Hello, please state your name and monthly income \n";
+    getline(cin, name);
+    cin>>income;
+}
---
+++ report card/draft1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	double math, phy, chem, bio, eng1, eng2, hist, geo, lang3,elec;
+	string name;
+	int grade;
+	char div;
+	cout << "Hello! Please enter the details of the student along with their marks.";
+	cout << "Name of the student: ";
+	getline(cin, name);
+	cout << "\nGrade: ";
+	cin >> grade;
+	cout << " \nDivision: ";
+	cin >> div;
+
+	return 0;
+}
